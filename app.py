6e39582e7d6b64767ca6bfd2c9572053ad03eee6
Timmyart <timmyjonesusn@gmail.com>
2025-10-24 15:15:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    # Render will set PORT for us. Default to 5000 for local.
+if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
