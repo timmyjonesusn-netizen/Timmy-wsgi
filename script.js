878f7ea45c28f3a2c44419797293bd1319486e3b
Timmyart <timmyjonesusn@gmail.com>
2025-10-25 05:12:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// ===== CONFIG =====
-const START_VALUE = 6;    // <-- put your real number here
-const DIGITS = 6;         // always show 6 digits like 000006
+// ===== SET THE NUMBER YOU WANT TO SHOW =====
+const START_VALUE = 40;   // <-- change this to whatever you want
+const DIGITS = 6;         // always show 6 digits like 000040
 
+// grab the display box
 const counterEl = document.getElementById("lux-counter");
 
-// builds the static digits
+// build those digits once and done
 function buildDigits(num) {
   const padded = num.toString().padStart(DIGITS, "0");
   counterEl.innerHTML = "";
@@ -17,8 +18,8 @@ function buildDigits(num) {
   }
 }
 
-// shimmer flex
+// shimmer forever because we’re glossy
 counterEl.classList.add("shimmer");
 
-// render once
+// render the static value
 buildDigits(START_VALUE);
