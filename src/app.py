2872ca420f9d3b6ffc3144ed764c9a5bcad883ff
Timmyart <timmyjonesusn@gmail.com>
2025-10-25 14:03:44 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+# tell Flask the templates and static folders are one level up
+app = Flask(__name__,
+            template_folder="../templates",
+            static_folder="../static")
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
